88c15921b8bbd4c798a29bc10c57657b814f21c6
Anas almograbi <anas2001mag@gmail.com>
2024-10-23 20:14:05 +0200

    Dashboard
    was complete  now wait for  action

--- NextJs2/next-js/app/ui/dashbord/card/card.tsx
+++ NextJs2/next-js/app/ui/dashbord/card/card.tsx
@@ -1,4 +1,16 @@
 import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 export default function Card() {
-  return <div>card</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <MdSupervisedUserCircle size={24} />
+        <span className={styles.title}>Total Users</span>
+      </div>
+      <div className={styles.amount}>10.273</div>
+      <div className={styles.percent}>
+        <span className={styles.positive}>12% </span>more than previous week
+      </div>
+    </div>
+  );
 }
